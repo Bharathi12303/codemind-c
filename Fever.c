64be2caf8fa0b63abf7db9f5e32ce6ce7aafad81
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-11-10 06:18:06 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x>98)     {         printf("YES");     }     else     {         printf("NO");     } }

--- Fever.c
+++ Fever.c
@@ -3,6 +3,12 @@ int main()
 {
     int x;
     scanf("%d",&x);
-    if(x>98) printf("YES");
-    else printf("NO");
+    if(x>98)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
 }
