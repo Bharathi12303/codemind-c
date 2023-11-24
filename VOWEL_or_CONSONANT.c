d56a22c1b00270ac2d3994ea3812884bb0d59415
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-11-24 09:43:50 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

--- VOWEL_or_CONSONANT.c
+++ VOWEL_or_CONSONANT.c
@@ -3,11 +3,12 @@ int main()
 {
     char ch;
     scanf("%c",&ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
     {
         printf("VOWEL");
     }
-    else if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-    printf("VOWEL");
-    else printf("CONSONANT");
+    else
+    {
+        printf("CONSONANT");
+    }
 }
