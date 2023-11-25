4d028e483f11d455509e6e330bb463fb9a453750
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-11-25 09:41:59 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(4*x<=1000) printf("YES");     else printf("NO"); }

--- Tickets.c
+++ Tickets.c
@@ -3,12 +3,6 @@ int main()
 {
     int x;
     scanf("%d",&x);
-    if(4*x<=1000)
-    {
-        printf("YES");
-    }
-    else
-    {
-        printf("NO");
-    }
+    if(4*x<=1000) printf("YES");
+    else printf("NO");
 }
