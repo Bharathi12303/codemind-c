44216396e34c0b19c7c6221fe3228e2b1c7286b7
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-11-25 09:48:10 +0530

    #include<stdio.h> int main() {     int x,y,z;     z=x/y;     scanf("%d%d",&x,&y);     if(x>y) printf("%d",x);     else printf("%d",y);
    }

--- Height.c
+++ Height.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int x,y;
+    int x,y,z;
+    z=x/y;
     scanf("%d%d",&x,&y);
-    if(x>y)
-    {
-        printf("%d",x);
-    }
-    else
-    {
-        printf("%d",y);
-    }
+    if(x>y) printf("%d",x);
+    else printf("%d",y);
+    
 }
