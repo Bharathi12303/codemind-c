b44b1a7f08555f1bc87a499d35e4e98f27122874
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-12-04 18:03:47 +0530

    #include<stdio.h> int main() {     int x1,y1,x2,y2,a,b;     scanf("%d%d%d%d",&x1,&y1,&x2,&y2);     a=x1+y1,b=x2+y2;     if(a>b) printf("%d",b);     else printf("%d",a); }

--- Interior_Design.c
+++ Interior_Design.c
@@ -4,6 +4,6 @@ int main()
     int x1,y1,x2,y2,a,b;
     scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
     a=x1+y1,b=x2+y2;
-    if(a<b) printf("%d",a);
-    else printf("%d",b);
+    if(a>b) printf("%d",b);
+    else printf("%d",a);
 }
