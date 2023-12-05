e200d52203df00dae710ec6a464b62699cc4627a
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-12-05 08:31:13 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     if(x>y) printf("%d",x-y);     else printf("%d",y-x); }

--- Donation.c
+++ Donation.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z,a;
+    int x,y;
     scanf("%d%d",&x,&y);
-    z=x-y,a=y-x;
-    if(x>y) printf("%d",z);
-    else printf("%d",a);
-    
+    if(x>y) printf("%d",x-y);
+    else printf("%d",y-x);
 }
