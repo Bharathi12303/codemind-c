11337e536a22f9d5010061dd9016f548a808159c
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-12-08 21:04:46 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     for(int i=a;i<=b;i++)     {         printf("%d ",i);     } }

--- 1_to_10.c
+++ 1_to_10.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,i;
-    scanf("%d",&a);
-    for(i=1;i<=10;i++)
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for(int i=a;i<=b;i++)
     {
         printf("%d ",i);
     }
