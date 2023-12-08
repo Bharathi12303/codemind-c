e4c531d1f87d7482a202b56b138dfc521fdf136e
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2023-12-08 21:12:31 +0530

    #include<stdio.h> int main() {     int a,b,i,t;     scanf("%d%d",&a,&b);     for(i=0;i<b;i++)     {         if(i>a) printf("%d %d %d ",i,i*i,i*i*i);     } }

--- N_-_S_-_C.c
+++ N_-_S_-_C.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-int main(){
-    int i,m,n;
-    scanf("%d%d",&m,&n);
-    for(int i=m+1;i<n;i++){
-        printf("%d %d %d
+int main()
+{
+    int a,b,i,t;
+    scanf("%d%d",&a,&b);
+    for(i=0;i<b;i++)
+    {
+        if(i>a) printf("%d %d %d
 ",i,i*i,i*i*i);
     }
 }
