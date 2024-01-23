3f14c3356e0407ff7c35fc602cee28418991700a
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2024-01-23 11:52:27 +0530

    #include<stdio.h> int main() {     float a,b,c;     scanf("%f",&a);     if(a<=199) b=a*1.20,c=b+100;     else if(a>=200 && a<400) b=a*1.50,c=b+100;     else if(a>=400 && a<600) b=a*1.80,c=b+(b*0.15);     else b=a*2.00,c=b+(b*0.15);     printf("%.2f",c); }

--- Current_bill.c
+++ Current_bill.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int u;
-    float b,t;
-    scanf("%d",&u);
-    if(u<200) b=u*1.2;
-    else if(u>=200 & u<400) b=u*1.5;
-    else if(u>=400 & u<600) b=u*1.8;
-    else b=u*2.0;
-    if(b>400) t=1.15*b;
-    else t=b+100;
-    printf("%.2f",t);
+    float a,b,c;
+    scanf("%f",&a);
+    if(a<=199) b=a*1.20,c=b+100;
+    else if(a>=200 && a<400) b=a*1.50,c=b+100;
+    else if(a>=400 && a<600) b=a*1.80,c=b+(b*0.15);
+    else b=a*2.00,c=b+(b*0.15);
+    printf("%.2f",c);
 }
