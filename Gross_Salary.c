e4c2ac2d25b62f30e82febfee1fe503be6885143
Bharathi12303 <147791698+Bharathi12303@users.noreply.github.com>
2024-01-23 11:37:33 +0530

    #include<stdio.h> int main() {     float bs,da,hra,gs;     scanf("%f",&bs);     if(bs<=10000)     {     da=bs*0.8,hra=bs*0.2;     }     else if(bs<=20000)     {         da=0.9*bs,hra=0.25*bs;     }     else     {         da=0.95*bs,hra=bs*0.3;     }     gs=bs+da+hra;     printf("%.2f",gs); }

--- Gross_Salary.c
+++ Gross_Salary.c
@@ -1,26 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int bs;
-    float da,hra;
-    scanf("%d",&bs);
+    float bs,da,hra,gs;
+    scanf("%f",&bs);
     if(bs<=10000)
     {
-        da=0.8*bs;
-        hra=0.2*bs;
-        printf("%.2f",bs+da+hra);
+    da=bs*0.8,hra=bs*0.2;
     }
     else if(bs<=20000)
     {
-        da=0.9*bs;
-        hra=0.25*bs;
-        printf("%.2f",bs+da+hra);
+        da=0.9*bs,hra=0.25*bs;
     }
-    else 
+    else
     {
-        da=0.95*bs;
-        hra=0.3*bs;
-        printf("%.2f",bs+da+hra);
+        da=0.95*bs,hra=bs*0.3;
     }
-    
+    gs=bs+da+hra;
+    printf("%.2f",gs);
 }
